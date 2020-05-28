dbbd590739334c340b637959d83e767ac08c3ad1
Ryan Duff <ryanduff613@gmail.com>
2020-05-28 16:30:36 -0700

    seed database succesfully, comment out manual seeding, working on seeding with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-division_list = [
-  ["Division1", "Advertising"],
-  ["Division2", "Marketing"],
-  ["Division3", "IT"],
-  ["Division4", "Sales"],
-  ["Division6", "Advertising"]
-]
+# division_list = [
+#   ["Division1", "Advertising"],
+#   ["Division2", "Marketing"],
+#   ["Division3", "IT"],
+#   ["Division4", "Sales"],
+#   ["Division6", "Advertising"]
+# ]
 
-division_list.each do |name, duty|
-  Division.create(name: name, duty: duty)
+# division_list.each do |name, duty|
+#   Division.create(name: name, duty: duty)
+# end
+
+Company.destroy_all
+
+10.times do |index|
+  Company.create!(:name Faker::name,
+    description: Faker::Lorem.sentence(20, false, 0).chop,
+    image_url: "site.com/here_is_a_picture_of_cardamom.jpg")
 end
+
+p "Created #{Spice.count} spices"
